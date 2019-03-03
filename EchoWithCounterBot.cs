5b7b0cc5498e1ef847949a4448440f9193bb888b
Veronika <veronika.kolesnikova@yahoo.com>
2019-03-02 20:38:47 -0500

    Initial commit

---
+++ EchoWithCounterBot.cs
@@ -0,0 +1,94 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading;
+using System.Threading.Tasks;
+using EchoBotWithCounter;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.BotBuilderSamples
+{
+    /// <summary>
+    /// Represents a bot that processes incoming activities.
+    /// For each user interaction, an instance of this class is created and the OnTurnAsync method is called.
+    /// This is a Transient lifetime service.  Transient lifetime services are created
+    /// each time they're requested. For each Activity received, a new instance of this
+    /// class is created. Objects that are expensive to construct, or have a lifetime
+    /// beyond the single turn, should be carefully managed.
+    /// For example, the <see cref="MemoryStorage"/> object and associated
+    /// <see cref="IStatePropertyAccessor{T}"/> object are created with a singleton lifetime.
+    /// </summary>
+    /// <seealso cref="https://docs.microsoft.com/en-us/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-2.1"/>
+    public class EchoWithCounterBot : IBot
+    {
+        // Supported LUIS Intents
+        public const string GreetingIntent = "Greeting";
+        public const string CancelIntent = "Cancel";
+        public const string HelpIntent = "Help";
+        public const string NoneIntent = "None";
+        public const string TranslateIntent = "Translate.Translate";
+
+        private const string WelcomeText = "This bot will introduce you to Microsoft Speech Translation. Let's get started";
+        private readonly EchoBotAccessors _accessors;
+        private readonly ILogger _logger;
+
+        /// <summary>
+        /// Key in the bot config (.bot file) for the LUIS instance.
+        /// In the .bot file, multiple instances of LUIS can be configured.
+        /// </summary>
+        public static readonly string LuisKey = "SpeechBotDemoV-a591";
+
+        /// <summary>
+        /// Services configured from the .bot file.
+        /// </summary>
+        private readonly BotServices _services;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EchoWithCounterBot"/> class.
+        /// </summary>
+        /// <param name="accessors">A class containing <see cref="IStatePropertyAccessor{T}"/> used to manage state.</param>
+        /// <param name="loggerFactory">A <see cref="ILoggerFactory"/> that is hooked to the Azure App Service provider.</param>
+        /// <seealso cref="https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-2.1#windows-eventlog-provider"/>
+        public EchoWithCounterBot(EchoBotAccessors accessors, ILoggerFactory loggerFactory)
+        {
+            if (loggerFactory == null)
+            {
+                throw new System.ArgumentNullException(nameof(loggerFactory));
+            }
+        }
+
+        /// <summary>
+        /// Every conversation turn for our Echo Bot will call this method.
+        /// There are no dialogs used, since it's "single turn" processing, meaning a single
+        /// request and response.
+        /// </summary>
+        /// <param name="turnContext">A <see cref="ITurnContext"/> containing all the data needed
+        /// for processing this conversation turn. </param>
+        /// <param name="cancellationToken">(Optional) A <see cref="CancellationToken"/> that can be used by other objects
+        /// or threads to receive notice of cancellation.</param>
+        /// <returns>A <see cref="Task"/> that represents the work queued to execute.</returns>
+        /// <seealso cref="BotStateSet"/>
+        /// <seealso cref="ConversationState"/>
+        /// <seealso cref="IMiddleware"/>
+        public async Task OnTurnAsync(ITurnContext context, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var recognizerResult = await _services.LuisServices[LuisKey].RecognizeAsync(context, cancellationToken);
+            var topIntent = recognizerResult?.GetTopScoringIntent();
+            if (topIntent != null && topIntent.HasValue && topIntent.Value.intent != "None")
+            {
+                await context.SendActivityAsync($"==>LUIS Top Scoring Intent: `'{topIntent.Value.intent}'`, Score: {topIntent.Value.score}\n");
+            }
+            else
+            {
+                var msg = "No LUIS intents were found.<br>" +
+                        "This sample is about identifying two user intents:<br>" +
+                        "`'Calendar.Add'`<br>" +
+                        "`'Calendar.Find'`<br>" +
+                        "Try typing `'Add Event'` or `'Show me tomorrow'`.<br>";
+                await context.SendActivityAsync(msg);
+            }
+        }
+    }
+}
---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBotWithCounter.sln -p:DeployOnBuild=true -p:PublishProfile=bottranslation123-Web-Deploy.pubxml -p:Password=pajeqt87HzowZJTSn6WnlAzrkQMDmRnrs9T5ovNkbpAe2nccSdLul53mHjhZ
+
